2371270d262b4a8ce6760ed542df120c15ac66ff
RMDG2001 <ricsmald@espol.edu.ec>
2023-07-09 18:07:22 -0500

    Registrar nuevocomprador

--- src/main/java/ec/edu/espol/proyectopoo/Comprador.java
+++ src/main/java/ec/edu/espol/proyectopoo/Comprador.java
@@ -5,12 +5,15 @@
 package ec.edu.espol.proyectopoo;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  * @author JOSUE
  */
 public class Comprador{
+    private static List<Comprador> compradoresRegistrados = new ArrayList<>();
     private String nombre;
     private String apellido;
     private String correoElectronico;
@@ -18,19 +21,42 @@ public class Comprador{
     private String correo;
     private String clave;
 
-    public Comprador(String nombre, String apellido, String correoElectronico, String organizacion, String correo, String clave) {
+    public Comprador(String nombre, String apellido, String organizacion, String correoElectronico, String clave) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.correoElectronico = correoElectronico;
         this.organizacion = organizacion;
-        this.correo = correo;
         this.clave = clave;
     }
     
     public void RegistarNuevoComprador(ArrayList<Comprador> compradores){
     
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Ingrese el Nombre:");
+        String nombre = sc.nextLine();
+        
+        System.out.println("Ingrese los Apellidos");
+        String apellidos = sc.nextLine();
+        
+        System.out.println("Ingrese la organización");
+        String organizacion = sc.nextLine();
+        
+        
+        System.out.println("Ingrese el Correo Electronico");
+        String correoElectronico= sc.nextLine();
+        
+        System.out.println("Ingrese la Clave");
+        String clave = sc.nextLine();
+        
+       
+        Comprador  c1 = new Comprador(nombre, apellidos, organizacion, correoElectronico, clave);
+        compradores.add(c1);
     }
     
+    
+   
+  }
+    
 
-}
+
